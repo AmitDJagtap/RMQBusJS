e468aac99b123fa797947d46f8422c031d779551
Amit J <amitjagtap@ayopop.com>
2018-12-14 17:26:19 +0530

    + broker class added

---
+++ api/interfaces/Broker.ts
@@ -0,0 +1,8 @@
+import Promise from 'bluebird';
+
+export interface Broker {
+    publish(topic:string, msg:any): Promise<any>;
+    subscribe(topic:string, callback:any): Promise<any>;
+    rpc(topic:string,message:any,callback:any):Promise<any>
+    unsubscribe(topic:string):void;
+}
