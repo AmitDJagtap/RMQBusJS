a86160ed6866f22398843e82e818b6a7feb0c8bc
Amit J <amitjagtap@ayopop.com>
2019-01-03 15:59:04 +0530

    + api for global ping + mfx

--- api/controllers/Misc_Controller.ts
+++ api/controllers/Misc_Controller.ts
@@ -1,6 +1,8 @@
  import util from "util";
  import RMQBroker from '../helpers/RMQBroker'
-import { func } from "joi";
+ import {
+   func
+ } from "joi";
 
  export function pingCheck(req: any, res: any, next: any) {
 
@@ -34,16 +36,27 @@ import { func } from "joi";
    });
  }
 
- export function greet(req:any,res:any, next :any) {
-  var name = req.swagger.params.name.value || 'Jessie';
-  var bus = new RMQBroker();
+ export function greet(req: any, res: any, next: any) {
+   var name = req.swagger.params.name.value || 'Jessie';
+   var bus = new RMQBroker();
 
-  bus.rpc("members.sayhi", {
-    data: name
-  }).then((res_data) => {
-    console.log(res_data.toString());
-    res.json({
-      "message": res_data.toString()
-    });
-  });
+   bus.rpc("members.sayhi", {
+     data: name
+   }).then((res_data) => {
+     console.log(res_data.toString());
+     res.json({
+       "message": res_data.toString()
+     });
+   });
+ }
+
+ export function pingAllServices(req: any, res: any, next: any) {
+   let name = req.swagger.params.name.value || 'Jessie';
+   let bus = new RMQBroker();
+   let testData = {
+     name: name,
+     pingfrom: "apigateway"
+   };
+   bus.publish("globalping", testData);
+   res.json(testData);
  }
