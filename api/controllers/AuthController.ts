1b79d404962e779d071e11ceb9fa956b648c9548
Baswaraj Sontakke <baswarajsontakke@ayopop.com>
2019-01-16 17:26:46 +0530

    add new api for verify account

--- api/controllers/AuthController.ts
+++ api/controllers/AuthController.ts
@@ -22,7 +22,23 @@ export function memberLogin(req:any,res:any,next: any){
     var data = {"headers":req.headers,"param": req.body,"apiPath":req.originalUrl,"method":req.method};
     var bus = new RMQBroker();
 
-    bus.rpc("membersAyopop.login", data).then((res_data) => {
+    bus.rpc("members.login", data).then((res_data) => {
+        console.log(res_data.toString());
+        res.json(JSON.parse(res_data.toString()));
+    });
+
+}
+/**
+ * This is the Service call to members verify account
+ * @param req
+ * @param res
+ * @param next
+ */
+export function verifyAccount(req:any,res:any,next: any){
+    var data = {"headers":req.headers,"param": req.body,"apiPath":req.originalUrl,"method":req.method};
+    var bus = new RMQBroker();
+
+    bus.rpc("members.verifyAccount", data).then((res_data) => {
         console.log(res_data.toString());
         res.json(JSON.parse(res_data.toString()));
     });
