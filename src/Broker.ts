47a26fb4e8ed19ff24526635d2d26c4e7edf6fd1
damitj07 <damitj07@gmail.com>
2019-05-13 16:05:37 +0530

    + mfx

--- src/Broker.ts
+++ src/Broker.ts
@@ -1,4 +1,4 @@
-export interface Broker {
+export interface IBroker {
   publish(topic: string, msg: any): Promise<any>;
   subscribe(topic: string, callback: any): Promise<any>;
   rpc(topic: string, message: any, callback: any): Promise<any>;
--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,65 @@
-import { Broker } from './Broker';
+import { IBroker } from './Broker';
 import * as amqp from 'amqplib';
 import { Connection, Channel, Replies } from 'amqplib';
 
-export default class RMQBroker implements Broker {
-  private static _conn: Connection;
-  private static _chan: Channel;
-  private static _functionsDir = __dirname + '/api/services/*.js';
-  private static _messageTtl: number;
-  private static _rpcTimeout: number;
+export default class RMQBroker implements IBroker {
+  private static CONN: Connection;
+  private static CHAN: Channel;
+  private static FUNCTIONS_FIR = __dirname + '/api/services/*.js';
+  private static MESSAGE_TTL: number;
+  private static RPC_TIMEOUT: number;
 
-  init(rmqConfig: any): Promise<any> {
+  public init(rmqConfig: any): Promise<any> {
     return new Promise<any>((res, rej) => {
-      RMQBroker._rpcTimeout = rmqConfig.rpcTimeout;
-      RMQBroker._messageTtl = rmqConfig.messageTtl;
+      RMQBroker.RPC_TIMEOUT = rmqConfig.rpcTimeout;
+      RMQBroker.MESSAGE_TTL = rmqConfig.messageTtl;
       amqp.connect(rmqConfig.url).then((connectedCon: Connection) => {
-        RMQBroker._conn = connectedCon;
-        RMQBroker._conn.createChannel().then((ch: Channel) => {
-          RMQBroker._chan = ch;
+        RMQBroker.CONN = connectedCon;
+        RMQBroker.CONN.createChannel().then((ch: Channel) => {
+          RMQBroker.CHAN = ch;
           res();
         });
       });
     });
   }
 
-  publish(topic: string, message: any): any {
-    let buff = Buffer.from(JSON.stringify(message));
-    let tempsplit = topic.split('.');
-    let routingKey = tempsplit[1];
-    let exchange = tempsplit[0];
-    let ok = RMQBroker._chan.publish(exchange, routingKey, buff);
+  public publish(topic: string, message: any): any {
+    const buff = Buffer.from(JSON.stringify(message));
+    const tempsplit = topic.split('.');
+    const routingKey = tempsplit[1];
+    const exchange = tempsplit[0];
+    const ok = RMQBroker.CHAN.publish(exchange, routingKey, buff);
     console.log('[x] Event Published : ' + topic);
     return ok;
   }
 
-  subscribe(topic: string, callback: any): Promise<any> {
+  public subscribe(topic: string, callback: any): Promise<any> {
     throw new Error('Method not implemented.');
   }
 
-  rpc(topic: string, message: any): Promise<any> {
+  public rpc(topic: string, message: any): Promise<any> {
     return new Promise<any>((res, rej) => {
       console.log('[x] RPC Invoked : ' + topic);
-      let buf = Buffer.from(JSON.stringify(message));
+      const buf = Buffer.from(JSON.stringify(message));
 
-      RMQBroker._chan
+      RMQBroker.CHAN
         .assertQueue('', { exclusive: true, durable: false, autoDelete: true })
         .then((q: Replies.AssertQueue) => {
-          let corr = generateUuid();
-          RMQBroker._chan.consume(
+            const corr = generateUuid();
+          RMQBroker.CHAN.consume(
             q.queue,
-            function(msg: any) {
-              if (msg && msg.properties.correlationId == corr) {
-                let consumerTag = msg.fields.consumerTag;
+            (msg: any)=> {
+              if (msg && msg.properties.correlationId === corr) {
+                const consumerTag = msg.fields.consumerTag;
                 console.log('[x] Response Received for : ' + q.queue);
                 res(msg.content);
-                RMQBroker._chan.cancel(consumerTag);
+                RMQBroker.CHAN.cancel(consumerTag);
               }
             },
             { noAck: true },
           );
 
-          RMQBroker._chan.sendToQueue(topic, buf, {
+          RMQBroker.CHAN.sendToQueue(topic, buf, {
             correlationId: corr,
             replyTo: q.queue,
           });
@@ -74,12 +74,12 @@ export default class RMQBroker implements Broker {
     });
   }
 
-  unsubscribe(topic: string): void {
+  public unsubscribe(topic: string): void {
     throw new Error('Method not implemented.');
   }
 }
 
-//temp for public code.
+// temp for public code.
 function generateUuid() {
   return Math.random().toString() + Math.random().toString() + Math.random().toString();
 }
