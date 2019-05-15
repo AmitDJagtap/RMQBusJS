ccec4eb45e0336a5742488efb06c5bb43b4c65da
damitj07 <damitj07@gmail.com>
2019-05-15 12:16:28 +0530

    + mfx + path root fix

--- src/FunctionRegistory.ts
+++ src/FunctionRegistory.ts
@@ -4,11 +4,12 @@ import { IConsumer as Consumer } from './Consumer';
 import { IResponder as Responder } from './Responder';
 import * as amqp from 'amqplib';
 import { Connection, Channel } from 'amqplib';
+const appRoot = require('app-root-path');
 
 export default class FunctionRegistry {
   private static CONN: Connection;
-  private respondersDir: string = path.join(__dirname, '..') + '/responders/*.js';
-  private consumersDir: string = path.join(__dirname, '..') + '/consumers/*.js';
+  private respondersDir: string = path.join(appRoot) + '/responders/*.js';
+  private consumersDir: string = path.join(appRoot) + '/consumers/*.js';
 
   public init(rmqConfig: any): Promise<any> {
     return new Promise<any>(res => {
